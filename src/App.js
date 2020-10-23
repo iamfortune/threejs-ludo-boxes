0fb028d8e5351ea4d7ac399f5c88c0047b828ae4
iamfortune <ikechifortune@gmail.com>
2020-10-23 05:58:48 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { useRef, useState, useMemo } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import * as THREE from "three";
+import one from "./assets/one.png";
+import two from "./assets/two.png";
+import three from "./assets/three.png";
+import four from "./assets/four.png";
+import five from "./assets/five.png";
+import six from "./assets/six.png";
 
-function App() {
+const Box = (props) => {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+
+  // Set up state for the hovered and active state
+  const [active, setActive] = useState(false);
+
+  // Rotate mesh every frame, this is outside of React without overhead
+  useFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+  });
+  
+  const texture = useMemo(() => new THREE.TextureLoader().load(five), []);
+  
   return (
-    <div>
-     <h1>Hello</h1>
-    </div>
+    <mesh
+    {...props}
+    ref={mesh}
+    scale={active ? [2, 2, 2] : [1.5, 1.5, 1.5]}
+    onClick={(e) => setActive(!active)}
+      >
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshBasicMaterial attach="material" transparent side={THREE.DoubleSide}>
+        <primitive attach="map" object={texture} />
+      </meshBasicMaterial>
+    </mesh>
+  );
+}
+
+const App = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[2.5, 0, 0]} />
+    </Canvas>
   );
 }
 
